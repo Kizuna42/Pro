cf471cf7eb451b5c1aea7016fdb041faa640733f
Kizuna Ishino <131520471+Kizuna42@users.noreply.github.com>
2025-02-24 15:30:54 +0900

    fi

--- src/app/Http/Controllers/RatingController.php
+++ src/app/Http/Controllers/RatingController.php
@@ -7,8 +7,6 @@ use App\Models\TradeStatus;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\TradeCompletedMail;
 
 class RatingController extends Controller
 {
@@ -31,21 +29,6 @@ class RatingController extends Controller
                 'rated_user_id' => $request->rated_user_id,
                 'rating' => $request->rating
             ]);
-
-            // 取引ステータスを取得
-            $tradeStatus = TradeStatus::with(['soldItem.item', 'soldItem.item.user'])->find($request->trade_status_id);
-
-            if (!$tradeStatus) {
-                throw new \Exception('取引情報が見つかりません');
-            }
-
-            // メール送信
-            $seller = $tradeStatus->soldItem->item->user;
-            Mail::to($seller->email)->send(new TradeCompletedMail(
-                $tradeStatus,
-                $tradeStatus->soldItem->item,
-                Auth::user()
-            ));
 
             DB::commit();
 
